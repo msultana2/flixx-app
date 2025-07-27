50f52363bf256a0bbe755ecdc7d5f468cfeaa9ed
Arjuman Sultana <arjuman.sultana86@gmail.com>
2025-07-27 13:19:51 -0700

    Page router & active link

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+// Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll(".nav-link");
+
+  links.forEach((link) => {
+    if (link.getAttribute("href") === global.currentPage) {
+      link.classList.add("active");
+    }
+  });
+}
+
+// Init App
+function init() {
+  switch (global.currentPage) {
+    case "/":
+    case "/index.html":
+      console.log("Home");
+      break;
+    case "/shows.html":
+      console.log("Shows");
+      break;
+    case "/search.html":
+      console.log("Search");
+      break;
+    case "/movie-details.html":
+      console.log("Movie details");
+      break;
+    case "/tv-details.html":
+      console.log("TV details");
+      break;
+  }
+
+  highlightActiveLink();
+}
+
+document.addEventListener("DOMContentLoaded", init);
