16f26ec43480982f5b9d1984669236d77b980312
Arjuman Sultana <arjuman.sultana86@gmail.com>
2025-07-28 13:20:04 -0700

    Display popular TV shows and add spinner

--- js/script.js
+++ js/script.js
@@ -34,7 +34,42 @@ async function displayPopularMovies() {
           </div>
         `;
 
-        document.getElementById('popular-movies').appendChild(div);
+    document.getElementById("popular-movies").appendChild(div);
+  });
+}
+
+async function displayPopularShows() {
+  const { results } = await fetchAPIData("tv/popular");
+
+  results.forEach((show) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+        <a href="tv-details.html?id=${show.id}">
+        ${
+          show.poster_path
+            ? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+            : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+        }
+            
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Aired: ${show.first_air_date}</small>
+            </p>
+          </div>
+    `;
+
+    document.getElementById("popular-shows").appendChild(div);
   });
 }
 
@@ -43,13 +78,25 @@ async function fetchAPIData(endpoint) {
   const API_KEY = "e246c554586333c38a890f00e821e6cc";
   const API_URL = "https://api.themoviedb.org/3/";
 
+  showSpinner();
+
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
 
   const data = await response.json();
 
+  hideSpinner();
+
   return data;
+}
+
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+
+function hideSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
 }
 
 // Highlight active link
@@ -72,6 +119,7 @@ function init() {
       console.log("Home");
       break;
     case "/shows.html":
+      displayPopularShows();
       console.log("Shows");
       break;
     case "/search.html":
