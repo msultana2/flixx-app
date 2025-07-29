e8d248f5a2df2a6a275b3a0cdc5183ab011f41dc
Arjuman Sultana <arjuman.sultana86@gmail.com>
2025-07-29 16:57:29 -0700

    Movie details page

--- js/script.js
+++ js/script.js
@@ -73,6 +73,75 @@ async function displayPopularShows() {
   });
 }
 
+// Display Movie Details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split("=")[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  const div = document.createElement("div");
+
+  div.innerHTML = `
+    <div class='details-top'>
+        <div>
+            ${
+              movie.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt=${movie.title}
+            />`
+                : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt=${movie.title}
+            />`
+            }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>${movie.vote_average.toFixed(
+                1
+              )} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>${movie.overview}</p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+            ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+            </ul>
+            <a href="${
+              movie.homepage
+            }" target="_blank" class="btn">Visit Movie Homepage</a>
+        </div>
+    </div>
+    <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget: </span>$${movie.budget.toLocaleString(
+              "en-US"
+            )}</li>
+            <li><span class="text-secondary">Revenue: </span>$${movie.revenue.toLocaleString(
+              "en-US"
+            )}</li>
+            <li><span class="text-secondary">Runtime: </span>${
+              movie.runtime
+            } minutes</li>
+            <li><span class="text-secondary">Status: </span>${
+              movie.release_date
+            }</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies
+            .map((company) => `<span>${company.name}</span>`)
+            .join(", ")}
+          </div>
+    </div>`;
+
+  document.querySelector("#movie-details").appendChild(div);
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   const API_KEY = "e246c554586333c38a890f00e821e6cc";
@@ -126,6 +195,7 @@ function init() {
       console.log("Search");
       break;
     case "/movie-details.html":
+      displayMovieDetails();
       console.log("Movie details");
       break;
     case "/tv-details.html":
