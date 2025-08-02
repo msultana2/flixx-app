c4bfe260d6cd37ff9fa60765d6fbea8406fb6995
Arjuman Sultana <arjuman.sultana86@gmail.com>
2025-08-01 22:41:24 -0700

    Display search results

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 const global = {
   currentPage: window.location.pathname,
   search: {
-    term: '',
-    type: '',
+    term: "",
+    type: "",
     page: 1,
-    totalPages: 1
-  }
+    totalPages: 1,
+  },
+  api: {
+    apiKey: "e246c554586333c38a890f00e821e6cc",
+    apiUrl: "https://api.themoviedb.org/3/",
+  },
 };
 
 async function displayPopularMovies() {
@@ -220,20 +224,59 @@ async function search() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
 
-  global.search.type = urlParams.get('type');
-  global.search.term = urlParams.get('search-term');
+  global.search.type = urlParams.get("type");
+  global.search.term = urlParams.get("search-term");
 
-  if(global.search.term !== '' && global.search.term !== null) {
-    // @todo - make request and display results
+  if (global.search.term !== "" && global.search.term !== null) {
+    const { results } = await searchAPIData();
+
+    if(results.length === 0) {
+      showAlert('No results found');
+      return;
+    }
+
+    displaySearchResults(results);
+
   } else {
-    showAlert('Please enter a search term');
+    showAlert("Please enter a search term", 'error');
   }
+}
+
+// Display search results
+function displaySearchResults(results) {
+  results.forEach(result => {
+      const div = document.createElement('div');
+      div.classList.add('card');
+      div.innerHTML = `
+        <a href="${global.search.type}-details.html?id=${result.id}">
+        ${ 
+          result.poster_path 
+            ? `<img 
+            src="https://image.tmdb.org/t/p/w500${result.poster_path}" 
+            class="card-img-top" 
+            alt="${global.search.type === 'movie' ? result.title : result.name}" />`
+            
+            : `<img 
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}" 
+            class="card-img-top" 
+            alt="${global.search.type === 'movie' ? result.title : result.name}" />`
+        }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_date : result.first_air_date}</small>
+          </p>
+        </div>
+    `;
+    document.querySelector('#search-results').appendChild(div);
+
+    });
 }
 
 // Display slider movies
 async function displaySlider() {
   const { results } = await fetchAPIData("movie/now_playing");
-  console.log(results);
 
   results.forEach((movie) => {
     const div = document.createElement("div");
@@ -279,13 +322,31 @@ function initSwiper() {
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
-  const API_KEY = "e246c554586333c38a890f00e821e6cc";
-  const API_URL = "https://api.themoviedb.org/3/";
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
 
   showSpinner();
 
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+
+  hideSpinner();
+
+  return data;
+}
+
+// Make request to search
+async function searchAPIData() {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
   );
 
   const data = await response.json();
@@ -338,10 +399,10 @@ function displayBackgroundImage(type, backgroundPath) {
 
 // Show alert
 function showAlert(message, className) {
-  const alertEl = document.createElement('div');
-  alertEl.classList.add('alert', className);
+  const alertEl = document.createElement("div");
+  alertEl.classList.add("alert", className);
   alertEl.appendChild(document.createTextNode(message));
-  document.querySelector('#alert').appendChild(alertEl);
+  document.querySelector("#alert").appendChild(alertEl);
 
   setTimeout(() => alertEl.remove(), 3000);
 }
